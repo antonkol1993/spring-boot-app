d8cb6233897c018386e883c4fb67ad8ffbc9789b
AntonPC <antonkol1993@gmail.com>
2024-12-26 19:42:42 +0300

    form work to POST ( addPlayerPost )

--- src/main/java/com/koleso/spring/controller/WelcomeController.java
+++ src/main/java/com/koleso/spring/controller/WelcomeController.java
@@ -1,11 +1,17 @@
 package com.koleso.spring.controller;
 
+import com.koleso.spring.dto.Player;
 import com.koleso.spring.service.PlayerService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @Controller
@@ -22,4 +28,21 @@ public class WelcomeController {
         modelAndView.setViewName("getPlayers");
         return modelAndView;
     }
+
+    @GetMapping("/add")
+    public ModelAndView addPlayerGet(ModelAndView modelAndView) {
+        Player player = new Player();
+        player.setName("dsadasdsa");
+        player.setAge(38);
+        modelAndView.addObject("players", player);
+        modelAndView.setViewName("form");
+        return modelAndView;
+    }
+    @PostMapping("/add")
+    public ModelAndView addPlayerPost(ModelAndView modelAndView) {
+
+        return modelAndView;
+    }
+
+
 }
