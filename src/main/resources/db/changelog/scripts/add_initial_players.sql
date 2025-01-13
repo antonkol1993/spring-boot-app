2c23e7c6416d5366c125209b9c08206774101102
AntonPC <antonkol1993@gmail.com>
2025-01-13 22:10:00 +0300

    edited initial scripts

--- src/main/resources/db/changelog/scripts/add_initial_players.sql
+++ src/main/resources/db/changelog/scripts/add_initial_players.sql
@@ -1,12 +1,12 @@
 -- liquibase formatted sql
 -- changeset antonio:1
 insert into player (age, id, name, position)
-values  (30, 1, 'messi', 'forward'),
-        (59, 2, 'Maradonna', null),
-        (35, 3, 'Antonio', 'forward'),
-        (35, 4, 'Henry', 'forward'),
-        (null, 5, 'Leonardo', 'defender');
-        (null, 5, 'Jamal', 'defender');
-        (null, 5, 'Rafinha', 'defender');
-        (null, 5, 'Vinchenso', 'defender');
-        (null, 5, 'Lewandovsky', 'defender');
+values (30, 1, 'messi', 'forward'),
+       (59, 2, 'Maradonna', null),
+       (35, 3, 'Antonio', 'forward'),
+       (35, 4, 'Henry', 'forward'),
+       (null, 5, 'Leonardo', 'defender'),
+    (null , 6, 'Jamal', 'defender'),
+    (null , 7, 'Rafinha', 'defender'),
+    (null , 8, 'Vinchenso', 'defender'),
+    (null , 9, 'Lewandovsky', 'defender');
