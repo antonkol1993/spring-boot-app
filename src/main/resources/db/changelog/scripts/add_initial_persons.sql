eca02ccb76bda022264e91416a8beb6fc02324e2
AntonPC <antonkol1993@gmail.com>
2025-01-13 22:06:08 +0300

    edited initial scripts

--- src/main/resources/db/changelog/scripts/add_initial_persons.sql
+++ src/main/resources/db/changelog/scripts/add_initial_persons.sql
@@ -1,8 +1,14 @@
 -- liquibase formatted sql
 -- changeset antonio:1
 insert into person (id, first_name, last_name, age)
-values (1,'Anton','Kolesnikov', 31),
-       (2,'Andrey','Narut', 33),
-       (3,'Vasya','Rostovsky', 12),
-       (4,'Petya','Pupkin', 56),
-       (5,'Bill','Milligan', 40);
+values (1, 'Anton', 'Kolesnikov', 31),
+       (2, 'Andrey', 'Narut', 33),
+       (3, 'Vasya', 'Rostovsky', 12),
+       (4, 'Petya', 'Pupkin', 56),
+       (5, 'Vinney', 'Puh', 48),
+       (6, 'Merilyn', 'Monroe', 99),
+       (7, 'Brad', 'Pitt', 40),
+       (8, 'Potap', 'Potapov', 44),
+       (9, 'Chelsia', 'Koppengaghen', 23,
+       (10, 'Willy', 'Johns', 59),
+       (11, 'Max', 'Holloway', 33);
--- src/main/resources/db/changelog/scripts/add_initial_players.sql
+++ src/main/resources/db/changelog/scripts/add_initial_players.sql
@@ -2,7 +2,11 @@
 -- changeset antonio:1
 insert into player (age, id, name, position)
 values  (30, 1, 'messi', 'forward'),
-        (null, 2, 'Maradonna', null),
+        (59, 2, 'Maradonna', null),
         (35, 3, 'Antonio', 'forward'),
         (35, 4, 'Henry', 'forward'),
-        (null, 5, 'Lahm', 'defender');
+        (null, 5, 'Leonardo', 'defender');
+        (null, 5, 'Jamal', 'defender');
+        (null, 5, 'Rafinha', 'defender');
+        (null, 5, 'Vinchenso', 'defender');
+        (null, 5, 'Lewandovsky', 'defender');
