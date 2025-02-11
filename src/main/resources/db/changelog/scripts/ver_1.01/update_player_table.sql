4311ee87cbc1aef8521377c3f8c2f510506d86ea
AntonPC <antonkol1993@gmail.com>
2025-02-11 15:06:59 +0300

    1. showTeam work correctly ( fix link of Team )

--- src/main/resources/db/changelog/scripts/ver_1.01/update_player_table.sql
+++ src/main/resources/db/changelog/scripts/ver_1.01/update_player_table.sql
@@ -5,6 +5,5 @@ ALTER TABLE `springboot`.`player`
     ADD COLUMN `image_url` TEXT NULL DEFAULT NULL AFTER `position_id`,
 ADD COLUMN `wiki_url` TEXT NULL DEFAULT NULL AFTER `image_url`;
 
--- changeset player:4
 ALTER TABLE `springboot`.`player`
     CHANGE COLUMN `rating` `rating` INT(11) NULL DEFAULT NULL ;
