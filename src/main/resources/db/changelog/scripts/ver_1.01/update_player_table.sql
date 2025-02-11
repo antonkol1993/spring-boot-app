adb001c5156dd0b535342614fba32789797a4f5f
AntonPC <antonkol1993@gmail.com>
2025-02-11 15:35:37 +0300

    1. check git

--- src/main/resources/db/changelog/scripts/ver_1.01/update_player_table.sql
+++ src/main/resources/db/changelog/scripts/ver_1.01/update_player_table.sql
@@ -5,5 +5,6 @@ ALTER TABLE `springboot`.`player`
     ADD COLUMN `image_url` TEXT NULL DEFAULT NULL AFTER `position_id`,
 ADD COLUMN `wiki_url` TEXT NULL DEFAULT NULL AFTER `image_url`;
 
+
 ALTER TABLE `springboot`.`player`
     CHANGE COLUMN `rating` `rating` INT(11) NULL DEFAULT NULL ;
